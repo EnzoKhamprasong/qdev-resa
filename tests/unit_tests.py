35e16b811ab2b68ee6dbb1915a25b76a160f8024
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-03-12 12:14:07 +0100

    Correction de la méthode test_bookMeetingRoom (Ajouts de '.__str__()')

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -3,10 +3,10 @@ from resa.resa import *
 
 class ResaUnitTests(unittest.TestCase):
   def test_bookMeetingRoom(self):
-    self.assertEqual(bookMeetingRoom(3), "Small")
-    self.assertEqual(bookMeetingRoom(15), "Medium")
-    self.assertEqual(bookMeetingRoom(35), "Large")
-    self.assertEqual(bookMeetingRoom(212), "Refused")
+    self.assertEqual(bookMeetingRoom(3).__str__(), "Small")
+    self.assertEqual(bookMeetingRoom(15).__str__(), "Medium")
+    self.assertEqual(bookMeetingRoom(35).__str__(), "Large")
+    self.assertEqual(bookMeetingRoom(212).__str__(), "Refused")
 
 if __name__ == '__main__':
   unittest.main()
