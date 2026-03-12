38d3e7fecbaef73e82b592457def39e99661b85c
Enzo Khamprasong <22503774@me.uvsq.fr>
2026-03-12 11:38:45 +0100

    Remplissage de la méthode ResaUnitTest

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -3,7 +3,10 @@ from resa.resa import *
 
 class ResaUnitTests(unittest.TestCase):
   def test_bookMeetingRoom(self):
-    self.assertEqual(True, False)
+    self.assertEqual(bookMeetingRoom(3), "Small")
+    self.assertEqual(bookMeetingRoom(15), "Medium")
+    self.assertEqual(bookMeetingRoom(35), "Large")
+    self.assertEqual(bookMeetingRoom(212), "Refused")
 
 if __name__ == '__main__':
   unittest.main()
